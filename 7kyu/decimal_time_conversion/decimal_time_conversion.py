70255f425f8654985bf5d448e9f540cd1586a6df
Mikhail Grechka <mishagreh@gmail.com>
2023-06-30 13:32:12 +0600

    decimal time conversion update

--- 7kyu/decimal_time_conversion/decimal_time_conversion.py
+++ 7kyu/decimal_time_conversion/decimal_time_conversion.py
@@ -5,12 +5,22 @@ qq = print
 
 
 def to_industrial(time):
+    """
+    Accepts either the time in minutes as an integer or a string of the format "h:mm".
+    Returns a float that represents decimal hours (e.g. 1.75 for 1h 45m).
+    """
+
     return round(time/60, 2) if isinstance(time, int) else round(int(time[-2:])/60, 2) + int(time[:-3])
 
 
 def to_normal(time):
-    x = round(time*100)
-    return '{}:{:02d}'.format(x*36//3600, round((x*36 % 3600)/60))
+    """
+    Accepts a float representing decimal time in hours.
+    Returns a string that represents standard time in the format "h:mm".
+    """
+
+    x = round(time*3600)
+    return '{}:{:02d}'.format(int(time), round(x % 3600/60))
 
 
 end = timer()
