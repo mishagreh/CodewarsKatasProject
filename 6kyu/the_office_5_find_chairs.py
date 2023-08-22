9adfa7a38deb00da4bd72ebe164dd1963dcb622d
Mikhail Grechka <mishagreh@gmail.com>
2023-08-22 13:12:27 +0600

    found free chairs (match/case solution added)

--- 6kyu/the_office_5_find_chairs.py
+++ 6kyu/the_office_5_find_chairs.py
@@ -21,11 +21,29 @@ def meeting(rooms, need):
 
     if sum(x) >= need:
         return x[:-1] + [x[-1] - sum(x) + need]
-
     return "Not enough!"
 
+# match/case statement solution
+#
+# def meeting(rooms, need):
+#     match not need:
+#         case True: return 'Game On'
+#         case False:
+#
+#             x = []
+#             for i in rooms:
+#                 if sum(x) < need:
+#                     match i[1] - len(i[0]) < 0:
+#                         case True: x.append(0)
+#                         case False: x.append(i[1] - len(i[0]))
+#
+#             match sum(x) >= need:
+#                 case True: return x[:-1] + [x[-1] - sum(x) + need]
+#                 case False: return "Not enough!"
 
-rooms, need = [['XXX', 5], ['', 1], ['XXXXX', 3], ['XX', 1], ['', 9], ['XXXXXXX', 10], ['X', 9], ['XXXXXXXXXX', 5], ['XXXXX', 7], ['XXXXXXXXXX', 10], ['XXXXXXX', 0], ['XXXX', 8]] , 2
+
+rooms, need = [['XXX', 5], ['', 1], ['XXXXX', 3], ['XX', 1], ['', 9], ['XXXXXXX', 10], ['X', 9], ['XXXXXXXXXX', 5],
+               ['XXXXX', 7], ['XXXXXXXXXX', 10], ['XXXXXXX', 0], ['XXXX', 8]] , 2
 print(meeting(rooms, need))
 
 end = timer()
